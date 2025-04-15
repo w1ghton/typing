510fda73e0b4d7d2ff1756ec817575759d10864c
Tim <you@example.com>
2025-04-15 09:14:24 +0300

    upd requirements

--- app/main.py
+++ app/main.py
@@ -3,5 +3,40 @@ import typing
 Vector = typing.List[float]
 
 
-def foo(vector: Vector):
-    print(vector)
+from datetime import datetime
+
+from pydantic import BaseModel, PositiveInt
+
+
+class User(BaseModel):
+    id: int
+    name: str = "John Doe"
+    signup_ts: datetime | None
+    tastes: dict[str, PositiveInt]
+    t: list[int]
+
+
+external_data = {
+    "id": 123,
+    "signup_ts": "2019-06-01 12:22",
+    "tastes": {
+        "wine": 9,
+        b"cheese": 7,
+        "cabbage": "1",
+    },
+    "t": [1, 2],
+}
+
+user = User(**external_data)
+
+print(user.id)
+# > 123
+print(user.model_dump())
+"""
+{
+    'id': 123,
+    'name': 'John Doe',
+    'signup_ts': datetime.datetime(2019, 6, 1, 12, 22),
+    'tastes': {'wine': 9, 'cheese': 7, 'cabbage': 1},
+}
+"""
